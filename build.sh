94c0d1fdb436ff74099db867405ecf65f6c60266
Ian Ross <ian@skybluetrades.net>
2014-06-10 17:01:50 +0200

    First attempt at basic Travis MacOS Haskell setup

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+c2hs --version
---
+++ setup_osx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+wget "http://www.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
+sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
+brew update
+cabal update
+export HSOPTS="-j --disable-documentation --disable-library-profiling --disable-executable-profiling"
+cabal install $HSOPTS cabal-install
+cabal install $HSOPTS c2hs
