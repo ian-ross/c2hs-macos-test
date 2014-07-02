84368c5960dd17113440a1664a05f6372fa7cf06
Ian Ross <ian@skybluetrades.net>
2014-07-02 20:43:25 +0200

    GHC CLang wrapper

--- setup_osx.sh
+++ setup_osx.sh
@@ -2,8 +2,8 @@
 wget "http://www.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 brew update
-cp ghc-clang-wrapper /usr/bin/ghc-clang-wrapper
-chmod 755 /usr/bin/ghc-clang-wrapper
-/usr/bin/ghc-clang-wrapper
+sudo cp ghc-clang-wrapper /usr/bin/ghc-clang-wrapper
+sudo chmod 755 /usr/bin/ghc-clang-wrapper
+sudo /usr/bin/ghc-clang-wrapper
 cabal update
 cabal install -j --disable-documentation --disable-library-profiling --disable-executable-profiling cabal-install
