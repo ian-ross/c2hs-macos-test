b88837c05baebff65b7a91d38f519314823a7bc7
Ian Ross <ian@skybluetrades.net>
2014-07-02 20:34:48 +0200

    XCode patching

--- setup_osx.sh
+++ setup_osx.sh
@@ -2,6 +2,8 @@
 wget "http://www.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 brew update
+cp ghc-clang-wrapper /usr/bin/ghc-clang-wrapper
+chmod 755 /usr/bin/ghc-clang-wrapper
+/usr/bin/ghc-clang-wrapper
 cabal update
-export HSOPTS="-j --disable-documentation --disable-library-profiling --disable-executable-profiling"
-cabal install $HSOPTS cabal-install
+cabal install -j --disable-documentation --disable-library-profiling --disable-executable-profiling cabal-install
